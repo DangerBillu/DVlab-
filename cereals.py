cebd0ad0c3a1bd83869806f8907a3bc6bafd3699
DangerBillu <161631802+DangerBillu@users.noreply.github.com>
2026-02-25 11:47:02 +0530

    cereals.py

---
+++ cereals.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("cereals.csv")
+print("printing first 5 rows")
+print(df.head())
+
+numericols = ["calories", "protein", "fat", "sodium", "fiber", "carbohydrates", "sugars", "vitamins"]
+df[numericols] = df[numericols].replace(-1, np.nan)
+for column in numericols:
+    mean = df[column].mean()
+    df[column] = df[column].fillna(mean)
+print("replaced")
+print(df[numericols].describe())
+
+for column in numericols:
+    median = df[column].median()
+    q1 = df[column].quantile(0.25)
+    q3 = df[column].quantile(0.75)
+    iqr = q3-q1
+
+    lower = q1 - 1.5*iqr
+    upper = q3 + 1.5*iqr
+
+    df[column] = np.where((df[column]<lower) | (df[column]>upper), median, df[column])
+    
+print("replaced")
+print(df[numericols].describe())
+
+print("printing first 5 rows")
+print(df.head())
